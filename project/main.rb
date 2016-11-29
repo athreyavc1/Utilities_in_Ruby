e56383703be9523205ef12e45cc038a85b037bc9
athreyavc <Kurt.wallander@aol.com>
2016-11-29 22:17:14 +0000

    First basic site

--- project/main.rb
+++ project/main.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 
-get '/hello' do 
- name = "Andy"
- "Hello #{name}"
+get '/' do
+ erb :home
 end
+__END__
+@@home
+<!doctype html>
+<html lang="en">
+   <head>
+       <title>Research on WARs</title>
+        <meta charset="utf-8">
+   </head>
+<body>
+<header>
+  <h1>WARS Humanity Fought</h1>
+  <nav>
+    <ul>
+      <li><a href="/" title="Home">Home</a></li>
+      <li><a href="/about" title="About">About</a></li>
+      <li><a href="/contact" title="Contact">Contact</a></li>
+    </ul>
+</header>
+<section>
+  <p> Welcome to the site </p>
+</section>
+</body>
+</html>
